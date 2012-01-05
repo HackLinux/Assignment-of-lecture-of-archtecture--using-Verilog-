899e2c472a6f36ea2b9292f472c31d380cfda7c9
officer <notbeafraidofanything@gmail.com>
2012-01-06 01:53:41 +0900

    second

--- archtecture/def.h
+++ archtecture/def.h
@@ -26,3 +26,22 @@
 `define RR_AND `OPCODE_W'b00110100 // Register-Register AND operation
 `define RR_OR `OPCODE_W'b00110101 // Register-Register OR operation
 `define RR_XOR `OPCODE_W'b00110110 // Register-Register XOR operation
+`define MR_CPA `OPCODE_W'b01000000 // Memory-Register CPA operation
+`define MR_CPL `OPCODE_W'b01000001 // Memory-Register CPL operation
+`define RR_CPA `OPCODE_W'b01000100 // Register-Register CPA operation
+`define RR_CPL `OPCODE_W'b01000101 // REgister-Register CPL operation
+`define SLA `OPCODE_W'b01010000 // Shift Left Arithmetic
+`define SRA `OPCODE_W'b01010001 // Shift Right Arithmetic
+`define SLL `OPCODE_W'b01010010 // Shigt Left Logical
+`define SRL `OPCODE_W'b01010011 // Shift Right Logical
+`define JMI `OPCODE_W'b01100001 // Jump MInus
+`define JNZ `OPCODE_W'b01100010 // Jump Not Zero
+`define JZE `OPCODE_W'b01100011 // Jump ZEro
+`define JUMP `OPCODE_W'b01100100 // JUMP
+`define JPL `OPCODE_W'b01100101 // Jump PLus
+`define JOV `OPCODE_W'b01100110 // Jump OVer flow
+`define PUSH `OPCODE_W'b01110000 // PUSH
+`define POP `OPCODE_W'b01110001 // POP
+`define CALL `OPCODE_W'b10000000 // CALL
+`define RET `OPCODE_W'b10000001 // RET
+`define SVC `OPCODE_W'b11110000 // SVC
